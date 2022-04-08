7d340b30b2df4b31849b77f34055e8f08b17abb1
michel muembo <michelmuemboilunga@gmail.com>
2022-04-08 22:53:12 +0200

    Initializing Project

---
+++ Index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pets Management</title>
+</head>
+<body>
+
+<!-- start includes page header -->
+<header id="header">
+
+</header>
+<!-- end includes page header -->
+
+<!--  start main -->
+<main id="main">
+   <h1> WELCOME TO PETS MANAGEMENT </h1>
+</main>
+<!-- end main -->
+
+<!-- start includes page footer -->
+<footer id="footer">
+    <h5>HAPPY HACKING</h5>
+    <div class="copyright">
+        &copy; Copyright <strong><span>Pets Management</span></strong>. All Rights Reserved
+    </div>
+</footer>
+<!-- end includes page footer -->
+</body>
+</html>
---
+++ README.md
@@ -0,0 +1,26 @@
+PETS MANAGEMENT
+===============
+
+Welcome to pets management system
+
+This System is under development, so you are welcome to contribute with your idea and code
+
+WHAT DOES THE SYSTEM
+====================
+* The System allows users to Authenticate by filling in their Credentials.
+* The System allows users to authenticate as Staff or Client.
+* The System allows users to afford services such as purchasing pets, registering pets, accomodating pets.
+* The System use an email service to contact the staff members.
+
+WHAT TECHNOLOGIES DOES THE SYSTEM USE
+====================================
+1. Markup Language
+   * Html 5
+2. Style Sheet
+   * Css 3
+   * Bootstrap
+3. Programming Languages
+   * Javascript (ES6)
+   * PHP 5 to 8
+4. Other techs
+   * Ajax
